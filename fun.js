11d7bb4e05f24d64e200cea142d0b16e01fccb93
civaca <gemelosvs@gmail.com>
2017-08-28 23:20:26 -0500

    first changes

---
+++ fun.js
@@ -0,0 +1,101 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    
+    /*function to change marign top*/
+        
+    
+    document.getElementById("searchinput").addEventListener("input", function newMargin(){document.getElementById("searchinput").setAttribute("Style","margin-top:0%");
+//     document.getElementById("lin").setAttribute("Style","margin-top:0%"); 
+                                                                                          
+        });
+    
+   
+        
+        
+        
+        
+       /*what to do with the imput*/ 
+        
+    document.getElementById("searchinput").addEventListener("change",function () {
+    
+        
+        
+        
+        var x = document.getElementById("searchinput").value;
+        
+  
+        
+    /*connectin with the Wiki API*/
+    var ajax = new XMLHttpRequest ();
+     ajax.open("GET","https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch="+x,true);
+     ajax.send();
+        
+    
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+             var newAjax = JSON.parse(this.responseText);
+            
+/*creating the results*/
+            var i=0;
+            while(i<newAjax.query.search.length) {
+                
+             
+                var newDiv= document.createElement("A");
+        newDiv.setAttribute("class","search");
+        newDiv.setAttribute("target","_blanck");
+
+     newDiv.setAttribute("href", "http://en.wikipedia.org/?curid="+newAjax.query.search[i].pageid);
+           
+       document.getElementById("results").appendChild(newDiv);
+      
+            
+            var newTitle= document.createElement("H2");
+         newTitle.setAttribute("class","search");
+        newTitle.innerHTML = newAjax.query.search[i].title;
+       newDiv.appendChild(newTitle);
+                
+          
+                var newText= document.createElement("P");
+        newText.setAttribute("class","search");
+           newText.innerHTML = newAjax.query.search[i].snippet;
+       newDiv.appendChild(newText);
+      
+                
+        
+            i++;
+           
+           
+   
+         };/*   closure of while*/
+            
+
+            
+        }/*closure of if*/
+    }; /*closure of onreadystate*/
+   
+    
+     
+    
+     
+        
+        
+        
+}); /* closure of change*/    
+
+
+    
+    
+     
+    
+}); /*closure of loaded*/
+  
+
+
+//   document.getElementById("searchinput").addEventListener("change",
+    
+      
+//        );/*closure of change*/
+    
